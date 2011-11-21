203fa09829a4f8356bfbb8392404b8ae867d6d71
jondynet <jondynet@gmail.com>
2011-11-21 11:28:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+一个刷票的脚本
+==============
+
+之前也有过几次刷投票或其他那啥的经验
+
+对于强大的验证码还是木有啥子好办法的。
+
+不过这次投票的目标网站是俺见过最狗血的了。。。看实现方式就知道了，哈哈
+
+这算是一次练习gevent的小应用吧。
+
+附几个代理服务器的网站，另外悼念一下俺的pubcc.com 这个是俺收集代理服务的小网站
+运行在GAE上，由于太自动化了而且自己也不常用代理，域名过期了忘了续费，呜呜。
+只能通过http://pubcc-com.appspot.com 访问了
+
+几个代理服务网站
+----------------
+
+* http://www.samair.ru/proxy/proxy-25.htm
+* http://proxy-list.org/en/index.php?sp=120&pp=any&pt=any&pc=any&ps=any
+* http://www.cybersyndrome.net/pla5.html
+* http://hidemyass.com/proxy-list/search-231187/2
+* http://www.digitalcybersoft.com/ProxyList/fresh-proxy-list.shtml
+* http://spys.ru/en/http-proxy-list/
+
+目标网站
+--------
+
+* http://www.xingfudaiyanren.com/Html/PhotoVote.asp?id=36&name=王易慧
+* http://www.xingfudaiyanren.com/Html/AmeavTotal.asp
+
---
+++ wangyihui.py
@@ -0,0 +1,53 @@
+#coding:utf-8
+import urllib2
+import gevent
+from gevent import monkey, queue
+monkey.patch_all()
+
+proxy_file = 'proxy.txt'
+
+refer = "http://www.xingfudaiyanren.com/Html/Main.asp?SortID=27&SortPath=0,27"
+url = "http://www.xingfudaiyanren.com/Html/PhotoVote.asp?id=36&name=王易慧"
+
+def vote(proxy, url, refer):
+    enable_proxy = True
+    proxy_handler = urllib2.ProxyHandler({"http" : 'http://%s' % proxy})
+    null_proxy_handler = urllib2.ProxyHandler({})
+     
+    if enable_proxy:
+        opener = urllib2.build_opener(proxy_handler)
+    else:
+        opener = urllib2.build_opener(null_proxy_handler)
+
+    urllib2.install_opener(opener)
+
+    req = urllib2.Request(url)
+    req.add_header("User-Agent", 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.18) Gecko/20110614 Firefox/3.6.18')
+    req.add_header( "Referer", refer)
+    res = urllib2.urlopen( req )
+    html = res.read()
+    res.close()
+    #return html
+    print proxy, html
+
+def worker(n):
+    while True:
+        proxy = q.get()
+        try:
+            vote(proxy, url, refer)
+        except:
+            pass
+        finally:
+            q.task_done()
+
+q = queue.JoinableQueue()
+num_worker_threads = 40
+
+proxys = open(proxy_file).readlines()
+for p in proxys:
+    q.put(p.strip())
+
+for i in range(num_worker_threads):
+    gevent.spawn(worker, i)
+
+q.join()
